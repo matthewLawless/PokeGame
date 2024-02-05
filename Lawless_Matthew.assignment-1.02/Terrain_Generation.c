f51fc0473a85ab0e11efd69eb5035173b54a1ec5
matthewLawless <mjlawless04@gmail.com>
2024-02-04 22:22:02 -0600

    Started 1.02.
    Created world object that holds that 401x401 array. Created a populateMap method that can match the gates of neighboring maps (messed with path generation method). Moved some code from main method into their own functions.

--- Lawless_Matthew.assignment-1.02/Terrain_Generation.c
+++ Lawless_Matthew.assignment-1.02/Terrain_Generation.c
@@ -8,7 +8,7 @@ int max(int a, int b);
 int min(int a, int b);
 struct Point findPair(struct Point p1, struct Point p2);
 void fillRectangle(struct Point p1, struct Point p2, char gameBoard[21][80], char c);
-void generatePaths(char gameBoard[21][80]);
+void generatePaths(char gameBoard[21][80], world_t* world, struct Point p);
 
 
 struct Point {
@@ -17,6 +17,141 @@ struct Point {
     int y;
 
 };
+
+void initPoint(struct Point* p, int x, int y){
+
+    p->x = x;
+    p->y = y;
+
+}
+
+//array entry
+typedef struct arrE {
+
+    int row;
+    int col;
+
+}arrE_t;
+
+void initArrE(arrE_t* a, struct Point p){
+
+    a->col = p.x + 200;
+    a->row = 200 - p.y;
+
+}
+
+typedef struct Map {
+
+    int eG;
+    int wG;
+    int nG;
+    int sG;
+    struct Point worldCoordinates;
+    char screen[21][80];
+
+
+}map_t;
+
+void initScreen(map_t* map){
+
+    
+    // map->screen = 
+    // char gameBoard[21][80] = map->screen;
+    //fill with Ns
+    int i, j;
+    for (i = 0; i < 21; i++){
+        for (j = 0; j < 80; j++){
+
+            map->screen[i][j] = '.';
+
+        }
+    }
+
+
+    //construct the frame of the board
+    
+    //top and bottom rows
+    for (j = 0; j < 80; j++){
+
+        map->screen[0][j] = '%';
+        map->screen[20][j] = '%';
+
+    }
+
+    //now do the columns
+    for (i = 0; i < 21; i++){
+
+        map->screen[i][0] = '%';
+        map->screen[i][79] = '%';
+
+        
+    }
+
+}
+
+typedef struct World {
+
+    map_t *worldMaps[401][401];
+
+}world_t;
+
+
+void populateMap(world_t* world, struct Point h){
+
+    arrE_t h1;
+    initArrE(&h1, h);
+    char gameBoard[21][80] = world->worldMaps[h1.row][h1.col]->screen;
+
+    // //Firstly, we need to check the n, e, s, w points to see if 
+    // //we need to match any of the gate coordinates or not
+    // struct Point n;
+    // initPoint(&n, p.x, p.y + 1);
+    // struct Point s;
+    // initPoint(&s, p.x, p.y - 1);
+    // struct Point e;
+    // initPoint(&e, p.x + 1, p.y);
+    // struct Point w;
+    // initPoint(&w, p.x - 1, p.y);
+
+    // arrE_t n1, s1, e1, w1;
+    // initArrE(&n1, n);
+    // initArrE(&s1, s);
+    // initArrE(&e1, e);
+    // initArrE(&w1, w);
+
+    // int nGate, sGate, eGate, wGate;
+
+
+
+    // if (world->worldMaps[n1.row][n1.col] != NULL){
+
+    //     nGate = world->worldMaps[n1.row][n1.col]->nG;
+
+    // }
+
+    // if (world->worldMaps[s1.row][s1.col] != NULL){
+
+    //     sGate = world->worldMaps[s1.row][s1.col]->sG;
+
+    // }
+
+    // if (world->worldMaps[e1.row][e1.col] != NULL){
+
+    //     eGate = world->worldMaps[e1.row][e1.col]->eG;
+
+    // }
+
+    // if (world->worldMaps[w1.row][w1.col] != NULL){
+
+    //     wGate = world->worldMaps[w1.row][w1.col]->wG;
+
+    // }
+
+    
+
+
+
+}
 
 void printPoint(struct Point p){
 
@@ -29,43 +164,7 @@ void printPoint(struct Point p){
 
 }
 
-
-
-int main(int argc, char *argv[]){
-
-    //init RNG
-    srand(time(NULL));
-
-    char gameBoard[21][80];
-    //fill with Ns
-    int i, j;
-    for (i = 0; i < 21; i++){
-        for (j = 0; j < 80; j++){
-
-            gameBoard[i][j] = '.';
-
-        }
-    }
-
-
-    //construct the frame of the board
-    
-    //top and bottom rows
-    for (j = 0; j < 80; j++){
-
-        gameBoard[0][j] = '%';
-        gameBoard[20][j] = '%';
-
-    }
-
-    //now do the columns
-    for (i = 0; i < 21; i++){
-
-        gameBoard[i][0] = '%';
-        gameBoard[i][79] = '%';
-
-        
-    }
+void generateTerrain(char gameBoard[21][80]){
 
     //Water
 
@@ -112,10 +211,107 @@ int main(int argc, char *argv[]){
     fillRectangle(q, q1, gameBoard, ':');
 
     // dumpBoard(gameBoard);
-
     generatePaths(gameBoard);
 
-    dumpBoard(gameBoard);
+}
+
+
+
+int main(int argc, char *argv[]){
+
+    //init RNG
+    srand(time(NULL));
+
+    // char gameBoard[21][80];
+    // //fill with Ns
+    // int i, j;
+    // for (i = 0; i < 21; i++){
+    //     for (j = 0; j < 80; j++){
+
+    //         gameBoard[i][j] = '.';
+
+    //     }
+    // }
+
+
+    // //construct the frame of the board
+    
+    // //top and bottom rows
+    // for (j = 0; j < 80; j++){
+
+    //     gameBoard[0][j] = '%';
+    //     gameBoard[20][j] = '%';
+
+    // }
+
+    // //now do the columns
+    // for (i = 0; i < 21; i++){
+
+    //     gameBoard[i][0] = '%';
+    //     gameBoard[i][79] = '%';
+
+        
+    // }
+
+
+
+    map_t m;
+    initScreen(&m);
+    dumpBoard(m.screen);
+
+
+
+
+//     //Water
+
+//     struct Point w1, w2;
+
+//     w1.x = (rand() % 77) + 1; //[1, 78]
+//     w1.y = (rand() % 17) + 1; //[1, 18]
+//     w2.x = (rand() % 77) + 1; //[1, 78]
+//     w2.y = (rand() % 17) + 1; //[1, 18]
+
+//     fillRectangle(w1, w2, gameBoard, '~');
+
+//     // dumpBoard(gameBoard);
+
+//     //Forest
+
+//     struct Point f1, f2;
+    
+//     f1.x = (rand() % 77) + 1; //[1, 78]
+//     f1.y = (rand() % 17) + 1; //[1, 18]
+//     f2.x = (rand() % 77) + 1; //[1, 78]
+//     f2.y = (rand() % 17) + 1; //[1, 18]
+
+//     fillRectangle(f1, f2, gameBoard, '^');
+
+//     // dumpBoard(gameBoard);
+
+//    //find two disjoint regions of tall grass
+//    //generate two points
+//    struct Point p, q;
+
+//     p.x = (rand() % 20) + 30; //[30, 50]
+//     p.y = (rand() % 6) + 7; //[7, 13]
+//     q.x = (rand() % 20) + 30;
+//     q.y = (rand() % 6) + 7;
+
+//     //figure out where the points are relative to each other in order to ensure
+//     //that when we generate their rectangle they are disjoint
+
+//     struct Point p1 = findPair(p, q);
+//     struct Point q1 = findPair(q, p);
+
+//     fillRectangle(p, p1, gameBoard, ':');
+//     fillRectangle(q, q1, gameBoard, ':');
+
+//     // dumpBoard(gameBoard);
+
+//     generatePaths(gameBoard);
+
+//     dumpBoard(gameBoard);
+
 
     
 
@@ -294,8 +490,54 @@ int max(int a, int b){
     }
 
 }
+//need to add parameter world_t world
+void generatePaths(char gameBoard[21][80], world_t* world, struct Point p){
 
-void generatePaths(char gameBoard[21][80]){
+    //Firstly, we need to check the n, e, s, w points to see if 
+    //we need to match any of the gate coordinates or not
+    // struct Point n;
+    // initPoint(&n, p.x, p.y + 1);
+    // struct Point s;
+    // initPoint(&s, p.x, p.y - 1);
+    // arrE_t n1, s1;
+    // initArrE(&n1, n);
+    // initArrE(&s1, s);
+    // int nGate, sGate;
+    // if (world->worldMaps[n1.row][n1.col] != NULL){
+
+    //     nGate = world->worldMaps[n1.row][n1.col]->nG;
+
+    // }
+
+    // if (world->worldMaps[s1.row][s1.col] != NULL){
+
+    //     sGate = world->worldMaps[s1.row][s1.col]->sG;
+
+    // }
+
+    
+    // struct Point e;
+    // initPoint(&e, p.x + 1, p.y);
+    // struct Point w;
+    // initPoint(&w, p.x - 1, p.y);
+    // arrE_t e1, w1;
+    // initArrE(&e1, e);
+    // initArrE(&w1, w);
+    // int eGate, wGate;
+    // if (world->worldMaps[e1.row][e1.col] != NULL){
+
+    //     eGate = world->worldMaps[e1.row][e1.col]->eG;
+
+    // }
+
+    // if (world->worldMaps[w1.row][w1.col] != NULL){
+
+    //     wGate = world->worldMaps[w1.row][w1.col]->wG;
+
+    // }
+
+
+
 
     char c = '#';
 
@@ -307,6 +549,42 @@ void generatePaths(char gameBoard[21][80]){
 
     //generate x value
     int x1 = (rand() % 78 + 1); // [1, 78]
+
+
+    struct Point e;
+    initPoint(&e, p.x + 1, p.y);
+    struct Point w;
+    initPoint(&w, p.x - 1, p.y);
+    arrE_t e1, w1;
+    initArrE(&e1, e);
+    initArrE(&w1, w);
+    // int eGate, wGate;
+    if (world->worldMaps[e1.row][e1.col] != NULL){
+
+        //switched eGate to y1
+        y1 = world->worldMaps[e1.row][e1.col]->eG;
+
+    }
+
+    if (world->worldMaps[w1.row][w1.col] != NULL){
+
+        //switched wGate to y2
+        y2 = world->worldMaps[w1.row][w1.col]->wG;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //draw straight line from (1, y1) to (x1, y1)
     int i;
@@ -341,6 +619,26 @@ void generatePaths(char gameBoard[21][80]){
     //choose two x values
     int a1 = (rand() % 78 + 1); // [1, 78]
     int a2 = (rand() % 78 + 1); // [1, 78]
+
+    struct Point n;
+    initPoint(&n, p.x, p.y + 1);
+    struct Point s;
+    initPoint(&s, p.x, p.y - 1);
+    arrE_t n1, s1;
+    initArrE(&n1, n);
+    initArrE(&s1, s);
+    // int nGate, sGate;
+    if (world->worldMaps[n1.row][n1.col] != NULL){
+
+        a1 = world->worldMaps[n1.row][n1.col]->nG;
+
+    }
+
+    if (world->worldMaps[s1.row][s1.col] != NULL){
+
+        a2 = world->worldMaps[s1.row][s1.col]->sG;
+
+    }
 
     //gen a y value
     int b1 = (rand() % 19) + 1; // [1, 19]
